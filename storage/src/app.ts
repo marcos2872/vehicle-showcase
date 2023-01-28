0f4663d9db6e078c5ef5e750c2526c0aabe9656c
MarcosBrito <njsudev5@hotmail.com>
2023-01-28 02:08:11 -0300

    get post delet de imagens no storage

--- storage/src/app.ts
+++ storage/src/app.ts
@@ -9,25 +9,46 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use(express.static('public'));
+app.use(express.static('public'))
 const PORT = 5190
 
 app.post('/images', multer(config).array('images'), (req: Request, res: Response) => {
-  const { id } = req.body
-  const dir = path.join(__dirname, '..', `public/${id}`)
-
-  const imgs = fs.readdirSync(dir)
-
-  const arrayImages = imgs.reduce((acc: string[], curr: string) => (
-    [...acc, `http://localhost:${PORT}/images/${id}/${curr}` ]
-    ), []);
-    console.log(arrayImages);
-    
-    
-  res.status(200).send('sucess')
+  try {
+    const { id } = req.body
+    const dir = path.join(__dirname, '..', `public/${id}`)
+  
+    const imgs = fs.readdirSync(dir)
+  
+    const arrayImages = imgs.reduce((acc: string[], curr: string) => (
+      [...acc, `http://localhost:${PORT}/images/${id}/${curr}` ]
+      ), []);
+      
+    res.status(200).json(arrayImages)
+  } catch (error) {
+    console.log(error)
+    res.status(500).send('Internal Server Error')
+  }
 })
 
-app.use('/images', express.static('public'));
+app.delete('/images', (req: Request, res: Response) => {
+  try {
+    const { id } = req.body
+    const dir = path.join(__dirname, '..', `public/${id}`)
+    if (!fs.existsSync(dir)) {
+      res.status(404).send('Not Found')
+    }
 
+    fs.rmdirSync(dir, {recursive: true })
+
+    res.status(200).send('Deleted')
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Internal Server Error')
+  }
+
+})
+
+app.use('/images', express.static('public'))
 
 app.listen(PORT, () => console.log(`Running server on port: ${PORT}`))
