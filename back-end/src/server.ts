1e0e7a27659841dfbb71c517cf3c5ad362a5ee36
Marcos Souza Brito <njsudev5@hotmail.com>
2023-02-22 13:41:56 -0300

    erro de type

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -5,7 +5,7 @@ import prisma from './Models/prisma';
 const PORT = process.env.PORT || 5180;
 prisma.$connect().then(() => {
   console.log('db connected');
-}).catch((error) => {
+}).catch((error: any) => {
   console.log('db not connected');
   console.log((error as Error).message);
 });
